02363fd05843a972be1e57b8d4a3576d8febb5f4
Narcikss <mdgamedeveloper@gmail.com>
2024-05-15 18:46:56 +0200

    prisma

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,21 @@
+import prisma from '../lib/prisma';
+
 import { currentUser } from "@clerk/nextjs/server"
+
+export const getStaticProps = async () => {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return {
+    props: { feed },
+    revalidate: 10,
+  };
+};
 
 const Home = async() => {
   const user = await currentUser()
