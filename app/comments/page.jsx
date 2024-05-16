31ae880e2b07303cbbf1ced3cb780460f75cdad2
Narcikss <mdgamedeveloper@gmail.com>
2024-05-16 20:36:55 +0200

    comments

--- app/comments/page.jsx
+++ app/comments/page.jsx
@@ -1,4 +1,8 @@
+import '@/styles/comments.css'
+
 import CommentBox from "@/components/CommentBox"
+
+import prisma from "@/lib/prisma"
 
 async function getComments() {
   const comments = await prisma.comment.findMany({
@@ -17,6 +21,7 @@ const Comments = async() => {
   return (
     <>
       <h1>Comments!</h1>
+      <section className='comments-container'>
       {
         comments.map(comment => {
           return (
@@ -26,10 +31,14 @@ const Comments = async() => {
             content={comment.content}
             likes={comment.likes}
             dislikes={comment.dislikes}
+            toWrap={
+              comment.content.length >= 29 ? true : false
+            }
             />
           )
         })
       }
+      </section>
     </>
   )
 }
--- components/CommentBox.jsx
+++ components/CommentBox.jsx
@@ -1,11 +1,51 @@
-const CommentBox = ({ authorName,  content, likes, dislikes}) => {
+'use client'
+
+import Image from "next/image"
+import { useEffect, useState } from "react"
+
+const CommentBox = ({ authorName,  content, likes, dislikes, toWrap}) => {
+  // const [commentContentWrap, setCommentContentWrap] = useState(false)
+
+  // useEffect(() => {
+  //   if (content.length >= 29) {
+  //     setCommentContentWrap(true)
+  //   }
+  // }, [])
+
+  console.log(authorName, toWrap)
+
   return (
-    <section className="comment-box">
-      <h3>{authorName}</h3>
-      <p>{content}</p>
-      <span>{likes}</span>
-      <span>{dislikes}</span>
-    </section>
+    <header className="comment-box">
+      <div className="comment-content-wrapper">
+        <h2>{authorName}</h2>
+        {
+          toWrap ?
+          <p style={{height: 'fit-content'}}>{content}</p>
+          :
+          <p style={{height: '20px'}}>{content}</p>
+        }
+      </div>
+      <section className="comments-interactive-section">
+        <div className="likes-wrapper">
+          <Image
+            src='heart.svg'
+            width={20}
+            height={20}
+            alt="like"
+          />
+          <span>{likes}</span>
+        </div>
+        <div className="dislikes-wrapper">
+          <Image
+            src='dislike.svg'
+            width={20}
+            height={20}
+            alt="like"
+          />
+          <span>{dislikes}</span>
+        </div>
+      </section>
+    </header>
   )
 }
 
